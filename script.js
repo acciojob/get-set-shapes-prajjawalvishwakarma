ffa1418f7fa4452a4535782775770d692cee7b2f
Prajjawal vishwakarma <115023930+prajjawalvishwakarma@users.noreply.github.com>
2025-10-02 15:53:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,52 +1,30 @@
 //complete this code
 class Rectangle {
-  private _width: number;
-  private _height: number;
-
-  constructor(width: number, height: number) {
+  constructor(width, height) {
     this._width = width;
     this._height = height;
   }
 
-  // Getter for width
-  get width(): number {
-    return this._width;
-  }
+  get width() { return this._width; }
+  set width(value) { this._width = value; }
 
-  // Getter for height
-  get height(): number {
-    return this._height;
-  }
+  get height() { return this._height; }
+  set height(value) { this._height = value; }
 
-  // Method to calculate area
-  getArea(): number {
+  getArea() {
     return this._width * this._height;
   }
 }
 
-// Square Class (inherits from Rectangle)
 class Square extends Rectangle {
-  constructor(side: number) {
-    super(side, side); // Call parent constructor with equal width and height
+  constructor(side) {
+    super(side, side);
   }
 
-  // Method to calculate perimeter
-  getPerimeter(): number {
-    return 4 * this.width; // side = width
+  getPerimeter() {
+    return 4 * this.width;
   }
 }
-
-// Example usage:
-const rect = new Rectangle(10, 5);
-console.log("Rectangle Width:", rect.width);
-console.log("Rectangle Height:", rect.height);
-console.log("Rectangle Area:", rect.getArea());
-
-const sq = new Square(7);
-console.log("\nSquare Side:", sq.width); // same as height
-console.log("Square Area:", sq.getArea()); // inherited method
-console.log("Square Perimeter:", sq.getPerimeter());
-
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
